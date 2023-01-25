7d5be505aee3549cdbd3d4df14a1bc57d07f34db
Joshua Cordero <joshcordero2134@gmail.com>
2023-01-25 14:37:56 -0800

    added text scraper as well as broad exception handling

--- helpers/parse_content.py
+++ helpers/parse_content.py
@@ -3,25 +3,44 @@ Parses text for tokenization as well as urls from given response content
 '''
 
 from lxml import html
+from lxml.html.clean import Cleaner
 
 import requests
 
+def scrape_text(content, base_url):
+    'takes in html string and base url returns text found in html'
+    try:
+        # define Cleaner and cleaner settings
+        cleaner = Cleaner(style=True, links=True, javascript=True, page_structure=True)
+        # clean html
+        cleaned_content = cleaner.clean_html(content)
+    
+    except Exception as e:
+        print('Failed cleaning: ', base_url, e)
+        cleaned_content = content
+    
+    try:
+        return html.fromstring(cleaned_content).text_content()
+    except Exception as e:
+        print('Failed parsing text from: ', base_url, e)
+        return ''
+
+
 def scrape_urls(content, base_url):
-    'takes in html string returns list of found urls'
+    'takes in html string and base url returns list of found urls'
 
-    # parse content and make links absolute for crawling
-    document = html.document_fromstring(content)
-    document.make_links_absolute(base_url)
+    try:
+        # parse content and make links absolute for crawling
+        document = html.document_fromstring(content)
+        document.make_links_absolute(base_url)
 
-    found_urls = []
+    except Exception as e:
+        print('Failed parsing links from: ', base_url)
+        return []
 
-    for (element, attribute, link, pos) in document.iterlinks():
-        found_urls.append(link)
-
-
-    return found_urls
+    return [link for element, attribute, link, pos in document.iterlinks()]
 
 if __name__ == '__main__':
     a = requests.get(r'https://github.com/joshc321/cs121-crawler4py')
     r = a.content
-    print(scrape_urls(r, 'https://github.com/joshc321/cs121-crawler4py'))
+    print(scrape_text(r, 'https://github.com/joshc321/cs121-crawler4py'))
