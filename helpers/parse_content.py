5f656a4e18f2b7937e75390004c11e3b7d98cf7b
Joshua Cordero <joshcordero2134@gmail.com>
2023-01-25 10:47:48 -0800

    added lxml dependency, started scrape_url function

--- helpers/parse_content.py
+++ helpers/parse_content.py
@@ -1,3 +1,20 @@
 '''
 Parses text for tokenization as well as urls from given response content
 '''
+
+from lxml import html
+
+def scrape_urls(content, base_url):
+    'takes in html string returns list of found urls'
+
+    # parse content and make links absolute for crawling
+    document = html.document_fromstring(content)
+    document.make_links_absolute(base_url)
+
+    found_urls = []
+
+    for (element, attribute, link, pos) in document.iterlinks():
+        found_urls.append(link)
+
+
+    return found_urls
