9b6d4c7e173e46eb5a062fc35a557a4b59086a97
Joshua Cordero <joshcordero2134@gmail.com>
2023-01-25 13:59:56 -0800

    scrape url test

--- helpers/parse_content.py
+++ helpers/parse_content.py
@@ -4,6 +4,8 @@ Parses text for tokenization as well as urls from given response content
 
 from lxml import html
 
+import requests
+
 def scrape_urls(content, base_url):
     'takes in html string returns list of found urls'
 
@@ -18,3 +20,8 @@ def scrape_urls(content, base_url):
 
 
     return found_urls
+
+if __name__ == '__main__':
+    a = requests.get(r'https://github.com/joshc321/cs121-crawler4py')
+    r = a.content
+    print(scrape_urls(r, 'https://github.com/joshc321/cs121-crawler4py'))
