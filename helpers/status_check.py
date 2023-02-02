fd5bdbcd2cc7819823fcafec0413ddd2a7de51de
Brian Le <brianle239@gmail.com>
2023-02-01 23:12:57 -0800

    Temporary ignore bad status codes

--- helpers/status_check.py
+++ helpers/status_check.py
@@ -1,17 +1,28 @@
 '''
 Checks status of response code and determines if it is a valid response
+
+Need to read into more status codes
+
 '''
 
-def isValidStatus(status):
-    # needs to be implimented
+def isValidStatus(resp):
+    # Server Should send 200 Status Codes
 
-    if (status == 204):
+    if (resp.status == 204):
         #No Content
         return False
+
+    # For now, ignore all 300 status code (redirect errors)
+    elif (resp.status >= 300 or resp.status < 400 ):
+        return False
+    
+    # Not Modified Error Code
     # elif (resp.status_code == 304):
     #     #Not Modified 
     #     return False
-    elif (status >= 600 and status <= 606):
-        #Cache Errors
+
+    # Error 4XX should have been dealt with already 
+    elif (resp.status >= 600 or resp.status_code <= 606):
+        # Cache Errors
         return False
     return True
