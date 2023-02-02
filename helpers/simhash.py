d81b0dc6e5f5038667ca502f1aee1168effe8a8e
Jacob Sommer <jacobgsommer@gmail.com>
2023-02-01 17:45:13 -0800

    Added simhash

---
+++ helpers/simhash.py
@@ -0,0 +1,58 @@
+HASH_LENGTH = 64 # length of hash's produced by builtin hash
+_DEBUG = False
+
+if _DEBUG:
+    HASH_LENGTH = 8
+    from nltk import FreqDist
+
+def simhash(tokens):
+    '''
+    Input: FreqDist of tokens
+    returns fingerprint as int
+    '''
+
+    fingerprint = 0
+
+    V = [0 for i in range(HASH_LENGTH)]
+
+    if _DEBUG:
+        fake_hash = {
+            'tropical': 0b01100001,
+            'fish': 0b10101011,
+            'include': 0b11100110,
+            'found': 0b00011110,
+            'environments': 0b00101101,
+            'around': 0b10001011,
+            'world': 0b00101010,
+            'including': 0b11000000,
+            'both': 0b10101110,
+            'freshwater': 0b00111111,
+            'salt': 0b10110101,
+            'water': 0b00100101,
+            'species': 0b11101110
+        }
+        tokens = FreqDist(['tropical', 'tropical', 'fish', 'fish', 'include', 'found', 'environments', 'around', 'world', 'including', 'both', 'freshwater', 'salt', 'water', 'species'])
+
+    for word, freq in tokens.items():
+        word_hash = hash(word)
+
+        if _DEBUG:
+            word_hash = fake_hash[word]
+
+        for i in range(HASH_LENGTH):
+            wordbit = (word_hash >> i) & 1
+
+            if wordbit == 1: # add weight
+                V[i] += freq
+            elif wordbit == 0: # subtract weight
+                V[i] -= freq
+
+    for i, vbit in enumerate(V):
+        if vbit > 0: # positive so 1 in fingerprint, add
+            fingerprint += (1 << i)
+
+    if _DEBUG:
+        print(V[::-1])
+        print(bin(fingerprint))
+
+    return fingerprint
