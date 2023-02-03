aca2bd0d47172b4e7a9bc562b3d46078a8edae09
Sean Fong <seancfong@gmail.com>
2023-02-03 09:54:26 -0800

    Add robot parser checking

--- helpers/url_check.py
+++ helpers/url_check.py
@@ -6,6 +6,9 @@ notes:
     '.' at the beginning of url is manditory
 '''
 import re
+from urllib.parse import urlparse, urlunsplit
+from urllib import parse
+from urllib import robotparser
 
 VALID_URL_PATTERN = re.compile(r'(?:(?P<subdomain>.*)\.)?(ics|cs|informatics|stat)\.uci\.edu$')
 
@@ -36,8 +39,35 @@ def get_subdomain(parsedurl):
     
     return ''
 
+def is_permitted(url):
+    '''
+        Detects whether the crawler is allowed to crawl the url or not
+
+        @arg url
+        @return bool
+    '''
+    try:
+        parser = robotparser.RobotFileParser()
+        user_agent = 'IR UW23 37183376,12247482,52163133,16453140'
+
+        parsed_url = urlparse(url)
+        parser.set_url(parse.urljoin(f'{parsed_url.scheme}://{parsed_url.netloc}', 'robots.txt'))
+        parser.read()
+
+        return parser.can_fetch(user_agent, parsed_url.path)
+    except Exception as e:
+        return False
+        
+
+
 if __name__ == '__main__':
     import urllib.parse
+
+    assert is_permitted('https://ics.uci.edu/bin/') == False
+    assert is_permitted('https://ics.uci.edu/bin/random.txt') == False
+    assert is_permitted('https://ics.uci.edu/~mpufal/') == False
+    assert is_permitted('https://ics.uci.edu/~thornton/') == True
+    assert is_permitted('https://ics.uci.edu/~fakeprof/') == True
 
     parsed = urllib.parse.urlparse('https://ics.uci.edu')
     print(is_valid_domain(parsed))
