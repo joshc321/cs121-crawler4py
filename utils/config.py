994728fa054cda7b5421fb91a650b8bd32c77744
Jacob Sommer <jacobgsommer@gmail.com>
2023-02-06 10:50:25 -0800

    Remove unused similarity threshold in config.ini

--- utils/config.py
+++ utils/config.py
@@ -16,6 +16,5 @@ class Config(object):
         self.seed_urls = config["CRAWLER"]["SEEDURL"].split(",")
         self.time_delay = float(config["CRAWLER"]["POLITENESS"])
         self.timeout = float(config["CRAWLER"]["TIMEOUT"])
-        self.similarity_threshold = float(config["CRAWLER"]["SIMILARITY_THRESHOLD"])
 
         self.cache_server = None
